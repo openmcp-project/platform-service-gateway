f122eb6966901ef476325ca49194fffdce428264
René Schünemann (SAP) <rene.schuenemann@sap.com>
2026-07-22 14:17:12 +0200

    enable fips check
    On-behalf-of: @SAP rene.schuenemann@sap.com
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>

--- cmd/platform-service-gateway/main.go
+++ cmd/platform-service-gateway/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"github.com/openmcp-project/controller-utils/pkg/fips"
 
 	"github.com/openmcp-project/platform-service-gateway/cmd/platform-service-gateway/app"
 )
 
 func main() {
+	fips.Verify(context.Background())
+
 	cmd := app.NewPlatformServiceGatewayCommand()
 
 	if err := cmd.Execute(); err != nil {
